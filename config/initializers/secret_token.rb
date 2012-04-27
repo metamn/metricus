59d3a66d59f80a1301781bc0bf828a3805803d75
metamn <bartus.csongor@gmail.com>
2012-04-27 09:43:20 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metricus::Application.config.secret_token = '2d703e0ef1a6f45ef5f732df6efc833f46d257e44db7aa9dba0810bfb7a8475f2e8ae32f1d032368be368b01f9d1784c53a92bb7f5e1b07f8288739cebc847d3'
